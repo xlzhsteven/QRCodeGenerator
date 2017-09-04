d9afb7c7ffcf197fc6008588e8d6799a95872f8a
Xiaolong Zhang <xlzhsteven@gmail.com>
2017-09-03 22:29:39 -0700

    Generate QR code

--- QRCodeGenerator/ViewController.swift
+++ QRCodeGenerator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageQRCode: UIImageView!
+    @IBOutlet weak var generateButton: UIButton!
+    
+    var qrCodeImage: CIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func performButtonAction(_ sender: Any) {
+        if qrCodeImage == nil {
+            guard let userEnteredText = textField.text, !userEnteredText.isEmpty else {
+                return
+            }
+            displayQRCodeImage(withText: userEnteredText)
+            generateButton.setTitle("Clear", for: .normal)
+        } else {
+            imageQRCode.image = nil
+            qrCodeImage = nil
+            generateButton.setTitle("Generate", for: .normal)
+        }
+    }
+    
+    func displayQRCodeImage(withText userEnteredText: String) {
 
+        let data = userEnteredText.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        
+        filter?.setValue(data, forKey: "inputMessage")
+        filter?.setValue("Q", forKey: "inputCorrectionLevel")
+        
+        qrCodeImage = filter?.outputImage
+        
+        let scaleX = imageQRCode.frame.size.width / qrCodeImage.extent.size.width
+        let scaleY = imageQRCode.frame.size.height / qrCodeImage.extent.size.height
+        let transform = CGAffineTransform(scaleX: scaleX, y: scaleY)
+        qrCodeImage = filter?.outputImage?.transformed(by: transform)
+        
+        if let qrImage = qrCodeImage {
+            imageQRCode.image = UIImage(ciImage: qrImage)
+            textField.resignFirstResponder()
+        }
+    }
+    
 }
 
